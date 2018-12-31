3f56134a52b2f155b81505ccaade242299bac24b
Gen-Arch <ymdtshm@gmail.com>
2019-01-01 07:13:10 +0900

    nemu

--- app.rb
+++ app.rb
@@ -6,7 +6,6 @@ require_relative "lib/application_helper"
 
 class FuckFish < Sinatra::Base
   set :server, :puma
-  set :public_folder, File.expand_path("../public", __dir__)
 
   before do
     request.script_name = "/fuck_fish"
