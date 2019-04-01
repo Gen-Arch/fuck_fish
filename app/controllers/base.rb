16df445100f382a6032f0d62461536aec804e487
gen <ymdtshm@gmail.com>
2019-04-01 23:31:25 +0900

    index 取得OOK

--- app/controllers/base.rb
+++ app/controllers/base.rb
@@ -50,6 +50,7 @@ class Base < Sinatra::Base
   options '*' do
     response.headers['Allow'] = 'HEAD,GET,PUT,POST,DELETE,OPTIONS'
     response.headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept'
+    response.headers["Access-Control-Allow-Origin"] = "*"
     status 200
   end
 end
